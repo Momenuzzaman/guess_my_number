4678c263e6ccfea46f19c838c27d0558b0bb1d9f
mdemon420 <mdeamon4@gmail.com>
2022-01-24 16:54:04 +0600

    add highscore

--- script.js
+++ script.js
@@ -12,6 +12,8 @@ let secretNumber = Math.round(Math.random() *20);
  let score = 20;
  document.querySelector('.score').textContent=score;
 
+ let highscore = 0;
+
 document.querySelector('.check').addEventListener('click', function(){
     const guess =Number( document.querySelector('.guess').value);
     console.log(guess);
@@ -24,6 +26,12 @@ document.querySelector('.check').addEventListener('click', function(){
       document.querySelector('.message').textContent = 'correct number!';
       document.querySelector('body').style.backgroundColor = '#60b347';
       document.querySelector('.number').style.width = '30rem';
+
+      if(score > highscore){
+        highscore = score;
+
+    document.querySelector('.highscore').textContent = highscore;
+      }
     }
     else if(guess > secretNumber){
         if(score > 1){
