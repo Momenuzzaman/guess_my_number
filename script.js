f96bf6f70821fd9ddeb0dd06905eb4c3f0588f46
mdemon420 <mdeamon4@gmail.com>
2022-01-24 12:47:14 +0600

    condition add

--- script.js
+++ script.js
@@ -9,6 +9,9 @@
 const secretNumber = Math.round(Math.random() *20);
 document.querySelector('.number').textContent = secretNumber;
 
+ let score = 20;
+ document.querySelector('.score').textContent=score;
+
 document.querySelector('.check').addEventListener('click', function(){
     const guess =Number( document.querySelector('.guess').value);
     console.log(guess);
@@ -20,9 +23,25 @@ document.querySelector('.check').addEventListener('click', function(){
       document.querySelector('.message').textContent = 'correct number!';
     }
     else if(guess > secretNumber){
-        document.querySelector('.message').textContent = 'Too High!';
+        if(score > 1){
+            document.querySelector('.message').textContent = 'Too High!';
+            score --;
+            document.querySelector('.score').textContent = score;
+        }
+        else{
+            document.querySelector('.message').textContent = 'lose the game';
+            document.querySelector('.score').textContent = 0;
+        }
     }
     else if(guess < secretNumber){
-        document.querySelector('.message').textContent = 'Too Low!';
+        if(score > 1){
+            document.querySelector('.message').textContent = 'Too Low!';
+            score --;
+            document.querySelector('.score').textContent = score;
+        }
+        else{
+            document.querySelector('.message').textContent = 'lose the game';
+            document.querySelector('.score').textContent = 0;
+        }
     }
 });
