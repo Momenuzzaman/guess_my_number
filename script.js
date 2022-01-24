281c28b1f9a251eaa1759a512710abfdac9ec6f3
mdemon420 <mdeamon4@gmail.com>
2022-01-24 13:04:21 +0600

    add style

--- script.js
+++ script.js
@@ -21,6 +21,8 @@ document.querySelector('.check').addEventListener('click', function(){
     }
     else if(guess === secretNumber){
       document.querySelector('.message').textContent = 'correct number!';
+      document.querySelector('body').style.backgroundColor = '#60b347';
+      document.querySelector('.number').style.width = '30rem';
     }
     else if(guess > secretNumber){
         if(score > 1){
