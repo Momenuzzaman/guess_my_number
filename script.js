215911a7c98f8c9030618cce90f37e25035d494d
mdemon420 <mdeamon4@gmail.com>
2022-01-24 11:58:00 +0600

    add button

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
 // 'use strict';
-const massage = document.querySelector('.message').textContent = 'correct number';
+// const massage = document.querySelector('.message').textContent = 'correct number';
 
-document.querySelector('.number').textContent = 10;
-document.querySelector('.score').textContent = 15;
+// document.querySelector('.number').textContent = 10;
+// document.querySelector('.score').textContent = 15;
 
-document.querySelector('.guess').value = 17;
+// document.querySelector('.guess').value = 17;
+
+
+document.querySelector('.check').addEventListener('click', function(){
+    const guess =Number( document.querySelector('.guess').value);
+    console.log(guess);
+
+    if(!guess){
+        document.querySelector('.message').textContent = 'Not a Number';
+    }
+});
