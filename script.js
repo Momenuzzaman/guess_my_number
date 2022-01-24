f12a1941601d9418448b49a187e9020d5016dda7
mdemon420 <mdeamon4@gmail.com>
2022-01-24 11:24:41 +0600

    add number,score,guess

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
 // 'use strict';
-const massage = document.querySelector('.message').textContent;
-console.log(massage);
+const massage = document.querySelector('.message').textContent = 'correct number';
+
+document.querySelector('.number').textContent = 10;
+document.querySelector('.score').textContent = 15;
+
+document.querySelector('.guess').value = 17;
