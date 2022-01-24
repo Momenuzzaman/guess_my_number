52a9b1113d7dd2b0ab8b05a5d74e56a7d1bbc7e6
mdemon420 <mdeamon4@gmail.com>
2022-01-24 16:34:10 +0600

    add again button

--- script.js
+++ script.js
@@ -6,8 +6,8 @@
 
 // document.querySelector('.guess').value = 17;
 
-const secretNumber = Math.round(Math.random() *20);
-document.querySelector('.number').textContent = secretNumber;
+let secretNumber = Math.round(Math.random() *20);
+
 
  let score = 20;
  document.querySelector('.score').textContent=score;
@@ -20,6 +20,7 @@ document.querySelector('.check').addEventListener('click', function(){
         document.querySelector('.message').textContent = 'Not a Number';
     }
     else if(guess === secretNumber){
+        document.querySelector('.number').textContent = secretNumber;
       document.querySelector('.message').textContent = 'correct number!';
       document.querySelector('body').style.backgroundColor = '#60b347';
       document.querySelector('.number').style.width = '30rem';
@@ -46,4 +47,21 @@ document.querySelector('.check').addEventListener('click', function(){
             document.querySelector('.score').textContent = 0;
         }
     }
+});
+document.querySelector('.again').addEventListener('click', function(){
+
+    score = 20;
+    secretNumber = Math.round(Math.random() *20);
+
+    document.querySelector('.number').textContent = '?';
+
+    document.querySelector('.message').textContent = 'Start guessing...';
+
+    document.querySelector('.score').textContent = score;
+
+    document.querySelector('.guess').value = '';
+
+    document.querySelector('.number').style.color = '#333';
+
+    document.querySelector('body').style.backgroundColor = '#222';
 });
